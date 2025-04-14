4797534e55833cb01ae3c7b1a1abc8bf68711abd
BLACKHAWK\MarcinD <marcin.domzal@zefcom.pl>
2025-04-14 22:53:21 +0200

    Error500 Pobieranie informacji o zalogowanym użytkowniku

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,30 +2,23 @@
     <v-app>
         <v-app-bar color="brand">
         <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-app-bar-title>RoC</v-app-bar-title>
-     
+        <v-app-bar-title>RoC</v-app-bar-title>   
         <template v-slot:append>
     <v-btn icon="mdi-heart"></v-btn>
-
     <v-btn icon="mdi-magnify"></v-btn>
-
     <v-btn icon="mdi-dots-vertical"></v-btn>
     <VBtn @click="toggleTheme" icon="mdi-theme-light-dark" title="Przełącz motyw"></VBtn>
   </template>
-
       </v-app-bar>
-
       <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer">
         <VList>
             <VListItem v-for="item in menuItems" :key="item.name" :title="item.name" :prepend-icon="item.icon" :to="item.url"></VListItem>
         </VList>
       </v-navigation-drawer>
-  
       <v-main>
         <div class="pa-4">
             <NuxtPage />
         </div>
-
       </v-main>
     </v-app>
   </template>
@@ -34,10 +27,12 @@
   import { useDisplay } from 'vuetify'
   import { useTheme } from 'vuetify'
 import { useStorage } from '@vueuse/core'
+
 const theme = useTheme()
   const { mobile } = useDisplay();
     const drawer = ref(null)
   const currentTheme = useStorage('currentTheme', 'light');
+  const userStore = useUserStore();
     const menuItems = [
         {
             name: 'Strona główna',
@@ -61,6 +56,7 @@ const theme = useTheme()
 
 onMounted(() =>{
 theme.global.name.value = currentTheme.value;
+userStore.loadLoggedInUser();
 });
   </script>
   
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,43 +6,43 @@ export default defineNuxtConfig({
       BASE_URL: 'http://localhost:5237', //replace with ENV: NUXT_PUBLIC_BASE_URL 
     },
   },
- devtools: { enabled: false },
- ssr: false,
+  devtools: { enabled: false },
+  ssr: false,
 
- build: {
- transpile: ['vuetify'],
-},
+  build: {
+    transpile: ['vuetify'],
+  },
 
-imports: {
-  dirs: ['stores'],
-},
+  imports: {
+    dirs: ['stores'],
+  },
 
- modules: [
-   (_options, nuxt) => {
-     nuxt.hooks.hook('vite:extendConfig', (config) => {
-       // @ts-expect-error
-       config.plugins.push(vuetify({ autoImport: true }))
-     })
-   },
-   '@pinia/nuxt',
-   '@vueuse/nuxt',
-   'nuxt-lodash',
-   'dayjs-nuxt'
- ],
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-lodash',
+    'dayjs-nuxt'
+  ],
 
- vite: {
-   vue: {
-     template: {
-       transformAssetUrls,
-     }, 
-   },
- },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 
- lodash: {
-  prefix: "_",
-  prefixSkip: false,
-  upperAfterPrefix: false,
-},
+  lodash: {
+    prefix: "_",
+    prefixSkip: false,
+    upperAfterPrefix: false,
+  },
 
- compatibilityDate: '2025-04-13',
+  compatibilityDate: '2025-04-13',
 })
