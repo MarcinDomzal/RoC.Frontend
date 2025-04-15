1faf037d895243c851df54bc229f8581743ed586
BLACKHAWK\MarcinD <marcin.domzal@zefcom.pl>
2025-04-15 17:52:32 +0200

    Logowanie

---
+++ components/LoginDialog.vue
@@ -0,0 +1,40 @@
+Kod
+<template>
+  <VDialog :model-value="show" persistent width="400" scroll-strategy="none">
+    <VCard class="py-4">
+    <VCardTitle class="text-center">Logowanie</VCardTitle>
+    <div v-if="userStore.$state.loading === true" class="pa-4 d-flex justify-center">
+    <VProgressCircular indeterminate></VProgressCircular>
+</div>
+    <VForm v-else @submit.prevent>
+        <VCardText>
+            <v-text-field class="mb-4" variant="outlined" v-model="viewModel.email" label="Email"></v-text-field>
+            <v-text-field class="mb-4" variant="outlined" v-model="viewModel.password" type="password" label="Hasło"></v-text-field>
+        </VCardText>
+        <VCardActions>
+            <v-btn class="mx-auto" color="primary" type="submit" variant="elevated">Zaloguj</v-btn>
+        </VCardActions>
+    </VForm>
+</VCard>
+</VDialog>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script setup>
+const userStore = useUserStore();
+const show = computed(() => {
+    return userStore.$state.isLoggedIn === false || userStore.$state.loading === true;
+});
+
+const viewModel = ref({
+    email: '',
+    password: ''
+});
+
+const submit = () => {
+    console.log(viewModel.value);
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,64 +1,63 @@
 <template>
     <v-app>
         <v-app-bar color="brand">
-        <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-app-bar-title>RoC</v-app-bar-title>   
-        <template v-slot:append>
-    <v-btn icon="mdi-heart"></v-btn>
-    <v-btn icon="mdi-magnify"></v-btn>
-    <v-btn icon="mdi-dots-vertical"></v-btn>
-    <VBtn @click="toggleTheme" icon="mdi-theme-light-dark" title="Przełącz motyw"></VBtn>
-  </template>
-      </v-app-bar>
-      <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer">
-        <VList>
-            <VListItem v-for="item in menuItems" :key="item.name" :title="item.name" :prepend-icon="item.icon" :to="item.url"></VListItem>
-        </VList>
-      </v-navigation-drawer>
-      <v-main>
-        <div class="pa-4">
-            <NuxtPage />
-        </div>
-      </v-main>
+            <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-app-bar-title>RoC</v-app-bar-title>
+            <template v-slot:append v-if="userStore.$state.isLoggedIn === true">
+                <v-btn icon="mdi-heart"></v-btn>
+                <v-btn icon="mdi-magnify"></v-btn>
+                <v-btn icon="mdi-dots-vertical"></v-btn>
+                <VBtn @click="toggleTheme" icon="mdi-theme-light-dark" title="Przełącz motyw"></VBtn>
+            </template>
+        </v-app-bar>
+        <v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer" v-if="userStore.$state.isLoggedIn === true">
+            <VList>
+                <VListItem v-for="item in menuItems" :key="item.name" :title="item.name" :prepend-icon="item.icon"
+                    :to="item.url"></VListItem>
+            </VList>
+        </v-navigation-drawer>
+        <v-main>
+            <div class="pa-4">
+                <NuxtPage v-if="userStore.$state.isLoggedIn === true"/>
+            </div>
+        </v-main>
+        <LoginDialog></LoginDialog>
     </v-app>
-  </template>
-  
-  <script setup>
-  import { useDisplay } from 'vuetify'
-  import { useTheme } from 'vuetify'
+</template>
+
+<script setup>
+import { useDisplay } from 'vuetify'
+import { useTheme } from 'vuetify'
 import { useStorage } from '@vueuse/core'
 
 const theme = useTheme()
-  const { mobile } = useDisplay();
-    const drawer = ref(null)
-  const currentTheme = useStorage('currentTheme', 'light');
-  const userStore = useUserStore();
-    const menuItems = [
-        {
-            name: 'Strona główna',
-            icon: 'mdi-home',
-            url: '/',
-        },
-        {
-            name: 'Monitorowanie strony',
-            icon: 'mdi-web-check',
-            url: '/urls',
-        }
-    ];
+const { mobile } = useDisplay();
+const drawer = ref(null)
+const currentTheme = useStorage('currentTheme', 'light');
+const userStore = useUserStore();
+const menuItems = [
+    {
+        name: 'Strona główna',
+        icon: 'mdi-home',
+        url: '/',
+    },
+    {
+        name: 'Monitorowanie strony',
+        icon: 'mdi-web-check',
+        url: '/urls',
+    }
+];
 
-    function toggleTheme () {
-        let newTheme = theme.global.current.value.dark ? 'light' : 'dark';
-  theme.global.name.value = newTheme;
-  currentTheme.value = newTheme;
+function toggleTheme() {
+    let newTheme = theme.global.current.value.dark ? 'light' : 'dark';
+    theme.global.name.value = newTheme;
+    currentTheme.value = newTheme;
 
 }
 
 
-onMounted(() =>{
-theme.global.name.value = currentTheme.value;
-userStore.loadLoggedInUser();
+onMounted(() => {
+    theme.global.name.value = currentTheme.value;
+    userStore.loadLoggedInUser();
 });
-  </script>
-  
-
-
+</script>
